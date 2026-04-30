7830d796c892cb6f6e8d8b910d5ca75a08b43bb8
Tyler Snedigar <tyler.snedigar@hulu.com>
2026-04-30 13:22:46 -0500

    code cleanup

--- src/core/types/messaging.ts
+++ src/core/types/messaging.ts
@@ -1,14 +1,6 @@
 import { EQState } from "./equalizer.ts";
 import { FilterParams } from "./filter.ts";
 
-// export type MessageType ="updateFilter"
-//   | "updatePreamp"
-//   | "addFilter"
-//   | "removeFilter"
-//   | "setState"
-//   | "updateEnabled"
-//   | "getState";
-
 // messages received by background script
 export type ControlMessageType = "getState" | "updateFilter" | "addFilter" | "removeFilter" | "updatePreamp" | "setFilters" | "updateEnabled";
 
@@ -24,14 +16,3 @@ export type BroadcastMessage = {
   type: BroadcastMessageType;
   payload: EQState;
 };
-
-// export type MessagePayload = FilterParams
-//   | FilterParams[]
-//   | number
-//   | boolean
-//   | string;
-
-// export type Message = {
-//   type: MessageType,
-//   payload?: MessagePayload
-// };
